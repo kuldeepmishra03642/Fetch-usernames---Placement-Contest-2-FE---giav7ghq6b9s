59f3ca28038ee412c420d8c0928d97ac34c84fc6
kuldeepmishra03642 <110096587+kuldeepmishra03642@users.noreply.github.com>
2022-10-09 15:26:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
-//code here 
+ const [data, setData]=useState([])
+const [input, setInput]=useState('')
+ 
  
 
 
-
+const changeInput=()=>{
+ setInput(e.target.value)
+}
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
