94751fbcbfc6bb42f36eb412a0c9c6c640336971
kuldeepmishra03642 <110096587+kuldeepmishra03642@users.noreply.github.com>
2022-10-09 15:50:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import '../styles/App.css';
 import { useState, useEffect } from 'react';
+
 const App = () => {
- const [data, setData]=useState([])
-const [input, setInput]=useState('')
- 
- 
-
-
-const changeInput=()=>{
- setInput(e.target.value)
+  const [name , setName] = useState('');
+  const [id ,setId] = useState(1)
+//code here 
+useEffect(()=>{
+  fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`).then((resp)=>resp.json()).then((data)=>{
+    console.log(data.name);
+    setName(data.name)
+  })
+} ,[id])
+const changeInput=(e)=>{
+  setId(e.target.value)
 }
+ 
+
+
+
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" onChange={(e)=>changeInput(e)} />
       <p id="name">{name}</p>
     </div>
   );
